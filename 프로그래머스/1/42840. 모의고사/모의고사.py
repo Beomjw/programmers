73e5ec67e9101516ca5d2cd69b7d2c8dc4f37d81
범종원 <129016121+Beomjw@users.noreply.github.com>
2024-11-19 19:34:34 +0900

    [level 1] Title: 모의고사, Time: 3.67 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,24 @@
+def solution(answers):
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    scores = [0, 0, 0]
+    
+    for i in range(len(answers)):
+        if answers[i] == p1[i % len(p1)]:
+            scores[0] += 1
+        if answers[i] == p2[i % len(p2)]:
+            scores[1] += 1
+        if answers[i] == p3[i % len(p3)]:
+            scores[2] += 1
+    
+    max_score = max(scores)
+    
+    result = []
+    
+    for i in range(len(scores)):
+        if scores[i] == max_score:
+            result.append(i + 1)
+            
+    return result
