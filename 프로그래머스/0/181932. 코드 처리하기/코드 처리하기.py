9ef97a1c1fcbee3590cd13f9f3126a15d904edee
범종원 <129016121+Beomjw@users.noreply.github.com>
2024-11-20 23:38:37 +0900

    [level 0] Title: 코드 처리하기, Time: 13.36 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,26 @@
+def solution(code):
+    answer = ''
+    # mode = 0
+    # code[idx] idx가 짝수일때만 ret 맨 뒤에 code[idx] 추가
+    # mode = 1
+    # code[idx] idx가 홀수일때만 ret 맨 뒤에 code[idx] 추가
+    
+    # mode = 0 으로 시작
+    
+    # abc1abc1abc
+    # a c  b  a c
+    mode = 0
+    
+    for i in range(len(code)):
+        if code[i] == '1':
+            mode = 1 - mode
+        else:    
+            if mode == 0 and i % 2 == 0:
+                    answer += code[i]
+            elif mode == 1 and i % 2 != 0:
+                    answer += code[i]
+    
+    if answer is '':
+        return "EMPTY"
+    else:
+        return answer
