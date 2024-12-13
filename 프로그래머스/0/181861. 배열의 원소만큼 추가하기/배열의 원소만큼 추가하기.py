d69bd36797f71582919f716815bf1ee4eb7f2cdc
범종원 <129016121+Beomjw@users.noreply.github.com>
2024-12-13 20:10:41 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.53 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
@@ -1,6 +1,8 @@
 def solution(arr):
     answer = []
-    for a in arr:
-        answer.extend([a] * a)
+    
+    for i in arr: # [5, 1, 4]
+        for j in range(i): # 0 1 2 3 4 / 0 / 0 1 2 3
+            answer.append(i) # [5, 5, 5, 5, 5, 1, 4, 4, 4, 4]
+    
     return answer
-
